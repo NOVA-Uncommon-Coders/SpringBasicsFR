f581b44b5952a9d6cf951cfada8974aa16873f95
D'Angelo Joyce <dangelomjoyce@gmail.com>
2017-03-14 22:43:51 -0400

    SpringBasics

--- src/main/java/com/novauc/Message.java
+++ src/main/java/com/novauc/Message.java
@@ -3,16 +3,20 @@ package com.novauc;
 /**
  * Created by dangelojoyce on 3/13/17.
  */
-public class Person {
+public class Message {
 
     int id;
     String name;
 
-    public Person (int id, String name){
+    public Message(int id, String name){
         this.id = id;
         this.name = name;
 
 
+    }
+
+    public Message(int id){
+        this.id = id;
     }
 
 }
